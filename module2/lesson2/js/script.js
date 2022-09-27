3a3544501e7be5077305ee393e6d9ca6df5d0cd8
Чумак Владимир <katya.12345>
2022-09-27 10:03:26 +0300

    two lessons of module2 passed

---
+++ module2/lesson2/js/script.js
@@ -0,0 +1,25 @@
+"use strict";
+
+let number = 5;
+const leftBorderWidth = 1;
+
+number = 10;
+console.log(number);
+
+const obj = {
+    a: 50
+};
+
+obj.a = 15;
+console.log(obj.a);
+
+console.log(name);
+var name = 'Ivan';
+
+{
+    let result = 50;
+    var visibilityTest = 100;
+}
+
+console.log(visibilityTest);
+console.log(result);
